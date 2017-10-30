f96a0dafbe6fa3e9cf6a6d79ca18f3b55f38cc6f
Fernando R Gonzáalez <rumbaut2288@gmail.com>
2017-10-30 19:55:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,5 +2,6 @@ function chocolateBars(){
   return ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 }
 function addElementToBeginningOfArray(array,element){
-  
+    var newArray = array.unshift(element);
+    return newArray;
 }

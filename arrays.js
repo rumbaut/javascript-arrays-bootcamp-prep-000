ea4898148d0e0aa07879de140fed2b8400b2b192
Fernando R Gonzáalez <rumbaut2288@gmail.com>
2017-10-30 19:58:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,3 +5,8 @@ function addElementToBeginningOfArray(array,element){
     array.unshift(element);
     return array;
 }
+function destructivelyAddElementToBeginningOfArray(array, element){
+  var newArray = array;
+  newArray.unshift(element);
+  return newArray;
+}
